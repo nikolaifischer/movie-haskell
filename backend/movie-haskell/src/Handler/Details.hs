8c609409c8e552e02315dd3221406d395883df28
Nikolai Fischer <nikolai.fischer91@gmail.com>
2017-08-19 13:20:00 +0200

    Adding (unstyled) Error Message when User is not found

--- backend/movie-haskell/src/Handler/Details.hs
+++ backend/movie-haskell/src/Handler/Details.hs
@@ -18,19 +18,30 @@ import           Yesod.Form.Bootstrap3
 successMessage :: String
 successMessage = "Your Recommendation was saved!"
 
+errorMessage :: Text
+errorMessage = "Could not find a User with this E-Mail Address"
+
 ----------------- GENERATING FORMS ------
 
--- This datatype serves no other purpose than wrapping a search-query
--- in the searchForm
+-- This datatype serves no other purpose than wrapping a reccomendation-query
+-- in the recommendationForm
 data UserQuery = UserQuery
           {email :: Text}
            deriving Show
 
 -- Applicative Form for Recommendations
-recommendationForm :: AForm Handler UserQuery
-recommendationForm = UserQuery
-            <$> areq textField (bfs ("Insert E-Mail of another User" :: Text))  Nothing
-
+recommendationForm ::  AForm Handler UserQuery
+recommendationForm  = UserQuery
+            <$> areq userField  (bfs ("Enter the E-Mail of a User" :: Text))  Nothing
+          where
+            -- Do some Input Validation: Is there an account for this E-Mail Address?
+            userField = checkM checkUser textField
+            checkUser :: Text -> Handler (Either Text Text)
+            checkUser input = do
+              maybeUser <- runDB $ selectFirst [UserIdent ==. input] []
+              return $ case maybeUser of
+                Nothing  -> Left ("This User does not have an Account on this platform" :: Text)
+                (Just _ ) -> Right input
 
 
 getDetailsR :: Int -> Handler Html
@@ -75,7 +86,7 @@ postDetailsR tmdbident = do
             let mail = email userQuery
             maybeUser <-  runDB $ selectFirst [UserIdent ==. mail] []
             let user =  case maybeUser of
-                        Nothing -> error "TODO: User not found error"
+                        Nothing -> error "Something went wrong with the Input Validation"
                         Just (Entity theid therest) -> theid
 
           -- Get the logged in User
@@ -91,6 +102,7 @@ postDetailsR tmdbident = do
                             Nothing   -> "User"
                             Just name -> name
 
+            -- Insert a new Movie Entity in the DB:
             themovieId <-  runDB $ Import.insert $ Import.Movie (pack (show tmdbident)) False userName
 
             let entity = Movie_User user themovieId
@@ -98,8 +110,9 @@ postDetailsR tmdbident = do
 
             setMessage $ toHtml $ successMessage
             redirect $ DetailsR tmdbident
-    --_ -> defaultLayout $(widgetFile "details")
-    _ -> redirect $ HomeR
+    _ ->    do
+              setMessage $ toHtml errorMessage
+              redirect $ DetailsR tmdbident
 
 
 -- | Search for movies with a query string.
